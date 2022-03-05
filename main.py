dd562b6af95431ecda0ac069bc00e707eaf82280
teredet <artem.vanzytler@gmail.com>
2022-03-05 11:25:22 +0200

    The print_info() function is implemented, which selects the necessary information from the json file and displays it in the terminal.

--- main.py
+++ main.py
@@ -11,9 +11,26 @@ def get_info(city='Kyiv', lang='en'):
     return requests.get(url=url, params=params).json()
 
 
+def print_info(weather):
+    city = weather['name']
+    country = weather['sys']['country']  # UA
+    weather_now = weather['weather'][0]['description'].capitalize()
+    temp = int(weather['main']['temp'])  # °C
+    pressure = weather['main']['pressure']  # hPa
+    humidity = weather['main']['humidity']  # %
+    wind_speed = int(weather['wind']['speed'] * 3.6)  # from  meter/sec to km/hour
+
+    print(f'''
+    {city},{country}
+    {temp}°C - {weather_now}
+    Humidity - {humidity}%
+    Wind speed - {wind_speed} km/hour
+    Atmospheric pressure - {pressure} hPa
+    ''')
+
 
 def main():
-    print(get_info('Кривий Ріг', 'ua'))
+    print_info(get_info('Кривий Ріг'))
 
 if __name__ == '__main__':
     main()
