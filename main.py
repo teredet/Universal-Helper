2e6b6ab0cf5c50c6360aaadf9c46087596536d97
teredet <artem.vanzytler@gmail.com>
2022-03-10 12:04:13 +0200

    Split the MainLayout class into CurrencyLayout and WeatherLayout classes.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.dropdown import DropDown
 
 import weather, currency
 
@@ -8,16 +9,19 @@ import weather, currency
 Window.size = (480, 853)
 
 
-
-class MainLayout(BoxLayout):
-    def print_weather(self):
+class WeatherLayout(BoxLayout):
+    def print_weather(self):           
         json = weather.get_json(self.city.text)
         if json['cod'] == 200:
             self.weather_result.text = weather.get_info(json)
         else:
             self.weather_result.text = 'Invalid city'
 
+
+class CurrencyLayout(BoxLayout):
+    
     def print_currency(self):
+
         res = ''
         if currency.get_json('uah'):
             for i in currency.get_info(currency.get_json('uah'),  'uah', {'usd', 'eur'}):
@@ -26,6 +30,13 @@ class MainLayout(BoxLayout):
             res = 'Invalid currency'
 
         self.curency_result.text = res
+
+class MainLayout(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(WeatherLayout())
+        self.add_widget(CurrencyLayout())
+    
 class HelperApp(App):
     def build(self):
         return MainLayout()
